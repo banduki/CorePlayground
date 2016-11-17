844c44068fa81af23c722261ae8ef7707ae945d8
Daniel Buki <git@banduki.com>
2016-11-17 15:55:09 -0600

    Initial commit

---
+++ TryNumber3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyMain
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Console.Write("\nWhat's your name? ");
+
+			Console.ReadLine();
+
+			var name = Console.ReadLine();
+
+			Console.WriteLine($"\n\nHello {name}!");
+
+			Console.Read();
+		}
+	}
+}
